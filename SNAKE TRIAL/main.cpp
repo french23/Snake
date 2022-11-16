d8c0b8525e3a0ebc890d5bfc569a64ff09a8196d
french23 <peterfstewart@outlook.com>
2022-11-16 00:03:38 -0600

    Fixed colision mostly
    Fixed the collision except for when the apple is smaller than the snake, this can be fixed by reversing the collision detection to detect the apple in the range of the snake, also got a basic snake follow system up and running, this was easy to do, all that was needed was to change the array to a vector.

--- SNAKE TRIAL/main.cpp
+++ SNAKE TRIAL/main.cpp
@@ -7,7 +7,7 @@ const int NUM_ROW = 800;
 const int NUM_COL = 800;
 
 const int SIZE = 25;
-const int APPLESIZE = 25;
+const int APPLESIZE = 26;
 
 enum Direction {UP, DOWN, LEFT, RIGHT};
 
@@ -18,17 +18,21 @@ int main(int argc, char **argv)
     SDL_Plotter g(NUM_ROW, NUM_COL);
     int R=20, G=20, B=255;
     //int xLoc = NUM_COL/2, yLoc = NUM_ROW/2;
-    int xLoc[length], yLoc[length];
+    vector<int> xLoc;
+    vector<int> yLoc;
     int prevX, prevY;
     Direction dir = RIGHT;
-    int speed = 150;
+    int speed = 90;
     int skip = 0, skip_val = 10;
 
-    xLoc[0] = NUM_COL/2;//head of snake
-    xLoc[1] = NUM_COL/2 - SIZE;
+    xLoc.push_back(NUM_COL/2);//head of snake
+    xLoc.push_back(NUM_COL/2 - SIZE);
 
-    yLoc[0] = NUM_ROW/2;
-    yLoc[1] = NUM_ROW/2;
+    yLoc.push_back(NUM_ROW/2);
+    yLoc.push_back(NUM_ROW/2);
+
+//    yLoc[0] = NUM_ROW/2;
+//    yLoc[1] = NUM_ROW/2;
 
     int appleX = rand() % 750;
     int appleY = rand() % 750;
@@ -36,10 +40,12 @@ int main(int argc, char **argv)
     int oldAppleY;
     bool appleEaten = false;
 
-    while(!g.getQuit()){
+    while(!g.getQuit())
+    {
 
         //user input
-        if(g.kbhit()){
+        if(g.kbhit())
+        {
             key = g.getKey();
             R = rand() % 255;
             G = rand() % 255;
@@ -49,15 +55,20 @@ int main(int argc, char **argv)
             oldAppleY = appleY;
 //            appleX = rand() % 785;
 //            appleY = rand() % 785;
-            switch(key){
-                case UP_ARROW   : dir = UP;
-                                  break;
-                case DOWN_ARROW : dir = DOWN;
-                                  break;
-                case LEFT_ARROW : dir = LEFT;
-                                  break;
-                case RIGHT_ARROW: dir = RIGHT;
-                                  break;
+            switch(key)
+            {
+            case UP_ARROW   :
+                dir = UP;
+                break;
+            case DOWN_ARROW :
+                dir = DOWN;
+                break;
+            case LEFT_ARROW :
+                dir = LEFT;
+                break;
+            case RIGHT_ARROW:
+                dir = RIGHT;
+                break;
 
             }
         }
@@ -66,44 +77,45 @@ int main(int argc, char **argv)
         /// Apple Collision ///
 
         // top left, reffrece point
-        if(xLoc[0] >= appleX && xLoc[0] <= appleX + APPLESIZE){
-           if(yLoc[0] >= appleY && yLoc[0] <= appleY + APPLESIZE){
-            appleEaten = true;
-            cout << "Apple twas Eaten! " << endl;
-           }
+        if((xLoc[0] >= appleX && xLoc[0] <= appleX + APPLESIZE)
+           and (yLoc[0] >= appleY && yLoc[0] <= appleY + APPLESIZE))
+        {
+                appleEaten = true;
+                cout << "Apple twas Eaten! - top left corner" << endl;
 
         }
         // top right
-        else if(xLoc[0] + SIZE >= appleX && xLoc[0] + SIZE <= appleX + APPLESIZE){
-           if(yLoc[0] >= appleY && yLoc[0] <= appleY + APPLESIZE){
+        else if(xLoc[0] + SIZE >= appleX && xLoc[0] + SIZE <= appleX + APPLESIZE
+                and (yLoc[0] >= appleY && yLoc[0] <= appleY + APPLESIZE))
+        {
             appleEaten = true;
-            cout << "Apple twas Eaten! " << endl;
-
-           }
+            cout << "Apple twas Eaten! - top right corner" << endl;
 
         }
-        // bottom left
-        else if(xLoc[0] >= appleX && xLoc[0] <= appleX + APPLESIZE){
-            //if(yLoc[0] + SIZE >= appleY && yLoc[0] + SIZE <= appleY + APPLESIZE)
-                {
+        // Bottom left
+        else if((xLoc[0] >= appleX and xLoc[0] <= appleX + APPLESIZE)
+                and ((yLoc[0]+SIZE) >= appleY and (yLoc[0]+SIZE) <= appleY + APPLESIZE))
+        {
                 appleEaten = true;
-                cout << "Apple twas Eaten! " << endl;
-            }
+                cout << "Apple twas Eaten! - bottom left corner" << endl;
         }
-        // bottum right
-        else if(xLoc[0] + SIZE >= appleX && xLoc[0] + SIZE <= appleX + APPLESIZE){
-            //if(yLoc[0] + SIZE >= appleY && yLoc[0] + SIZE <= appleY + APPLESIZE)
-                {
+        // Bottom Right
+        else if(((xLoc[0]+SIZE) >= appleX and (xLoc[0]+SIZE) <= appleX + APPLESIZE)
+                and ((yLoc[0]+SIZE) >= appleY and (yLoc[0]+SIZE) <= appleY + APPLESIZE))
+        {
                 appleEaten = true;
-                cout << "Apple twas Eaten! " << endl;
-            }
+                cout << "Apple twas Eaten! - bottom right corner" << endl;
         }
 
 
-        if(appleEaten){
+        // Run
+        if(appleEaten)
+        {
             /// Apple Erase
-            for(int y = 0; y < APPLESIZE; y++){
-                for(int x = 0; x < APPLESIZE; x++){
+            for(int y = 0; y < APPLESIZE; y++)
+            {
+                for(int x = 0; x < APPLESIZE; x++)
+                {
                     g.plotPixel(appleX + x, appleY + y, 255,255,255);
                 }
             }
@@ -118,15 +130,20 @@ int main(int argc, char **argv)
 
             /// increment lenght //
             length++;
+            xLoc.push_back(prevX);
+            yLoc.push_back(prevY);
         }
 
 
 
 
         /// Snake erase
-        for (int i = 0; i < length; i++){
-            for(int y = 0; y < SIZE; y++){
-                for(int x = 0; x < SIZE; x++){
+        for (int i = 0; i < length; i++)
+        {
+            for(int y = 0; y < SIZE; y++)
+            {
+                for(int x = 0; x < SIZE; x++)
+                {
                     g.plotPixel(xLoc[i] + x, yLoc[i] + y, 255,255,255);
 
                 }
@@ -137,27 +154,36 @@ int main(int argc, char **argv)
 
         //Copy Cell Locations
 
-        for(int i = length-1; i > 0; i--){
+        for(int i = length-1; i > 0; i--)
+        {
             xLoc[i] = xLoc[i-1];
             yLoc[i] = yLoc[i-1];
         }
 
-        switch(dir){
-            case RIGHT: xLoc[0]+= SIZE;
-                        break;
-            case LEFT : xLoc[0]-= SIZE;
-                        break;
-            case UP   : yLoc[0]-= SIZE;
-                        break;
-            case DOWN : yLoc[0]+= SIZE;
-                        break;
+        switch(dir)
+        {
+        case RIGHT:
+            xLoc[0]+= SIZE;
+            break;
+        case LEFT :
+            xLoc[0]-= SIZE;
+            break;
+        case UP   :
+            yLoc[0]-= SIZE;
+            break;
+        case DOWN :
+            yLoc[0]+= SIZE;
+            break;
 
         }
 
         //Draw
-        for(int i = 0; i < length; i++){
-            for(int y = 0; y < SIZE; y++){
-                for(int x = 0; x < SIZE; x++){
+        for(int i = 0; i < length; i++)
+        {
+            for(int y = 0; y < SIZE; y++)
+            {
+                for(int x = 0; x < SIZE; x++)
+                {
                     g.plotPixel(xLoc[i] + x, yLoc[i] + y, R,G,B);
 
                 }
@@ -165,8 +191,10 @@ int main(int argc, char **argv)
         }
 
 
-        for(int y = 0; y < APPLESIZE; y++){
-            for(int x = 0; x < APPLESIZE; x++){
+        for(int y = 0; y < APPLESIZE; y++)
+        {
+            for(int x = 0; x < APPLESIZE; x++)
+            {
                 g.plotPixel(appleX + x, appleY + y);
             }
         }
