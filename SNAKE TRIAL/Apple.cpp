14f820ec211a303346bc90aa579888db3aa2a750
Renzenar <118459019+Renzenar@users.noreply.github.com>
2022-12-01 22:49:15 -0600

    Apple is usable, more complex coming

--- SNAKE TRIAL/Apple.cpp
+++ SNAKE TRIAL/Apple.cpp
@@ -92,11 +92,40 @@ void Apple::drawApple(SDL_Plotter& g){
             g.plotPixel(appSeg.getX() + x, appSeg.getY() + y, R, G, B);
         }
     }
+    //Apple Corners + Leaf
+    for(int y = 0; y < 5; y++){
+        for(int x = 0; x < 5; x++){
+            g.plotPixel(appSeg.getX() + x, appSeg.getY() + y, 255, 255, 255);
+            g.plotPixel(appSeg.getX() + x+20, appSeg.getY() + y, 255, 255, 255);
+            g.plotPixel(appSeg.getX() + x, appSeg.getY() + y+20, 255, 255, 255);
+            g.plotPixel(appSeg.getX() + x+20, appSeg.getY() + y+20, 255, 255, 255);
+            g.plotPixel(appSeg.getX() + x+14, appSeg.getY() - y-10, 92, 169, 4);
+        }
+    }
+    //Apple Stem
+    for(int x = 0; x < 5; x++){
+        for(int y = 0; y < 10; y++){
+            g.plotPixel(appSeg.getX() + x+10, appSeg.getY() - y, 128, 64, 0);
+        }
+    }
+
 }
 void Apple::eraseApple(SDL_Plotter& g){
     for(int y = 0; y < appleSize; y++){
         for(int x = 0; x < appleSize; x++){
             g.plotPixel(appSeg.getX() + x, appSeg.getY() + y, 255, 255, 255);
+        }
+    }
+    //Apple Stem Erase
+    for(int x = 0; x < 5; x++){
+        for(int y = 0; y < 10; y++){
+            g.plotPixel(appSeg.getX() + x+10, appSeg.getY() - y, 255, 255, 255);
+        }
+    }
+    //Apple Leaf Erase
+    for(int y = 0; y < 5; y++){
+        for(int x = 0; x < 5; x++){
+            g.plotPixel(appSeg.getX() + x+14, appSeg.getY() - y-10, 255, 255, 255);
         }
     }
 }
